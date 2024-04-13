92e2a7fa12f7915d9d94ab194e6faafeb2245cfa
nikhil-pp <nikhil_pp@apple.com>
2024-04-13 12:39:27 +0530

    Progress : Buttons and hero text

--- gym/src/components/Hero/Hero.js
+++ gym/src/components/Hero/Hero.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './hero.css'
 import Header from '../Header/Header'
+import Heart from '../../images/heart1.png'
 export default function Hero() {
   return (
     <div className='hero'>
@@ -8,10 +9,48 @@ export default function Hero() {
             <Header/>
             <div className='the-best-ad'>
                 <div></div>
-                <span>The Best..!!,Wake to reality guys</span>
+                <span>The Best!! Wake to reality guys</span>
+            </div>
+            <div className='hero-text'>
+              <div>
+                <span className='stroke-text'>Shape </span>
+                <span>Your</span>
+              </div>
+              <div>
+                <span>Ideal body</span>
+              </div>
+              <div>
+                <span>If you want something you've never had, you must be willing to do something you've never done</span>
+              </div>
+            </div>
+            <div className='figures'>
+              <div>
+                <span>+220</span>
+                <span>Expert coach</span>
+              </div>
+              <div>
+                <span>+1000</span>
+                <span>Members joined</span>
+              </div>
+              <div>
+                <span>+50</span>
+                <span>Fitness program</span>
+              </div>
+            </div>
+
+            <div className='hero-buttons'>
+              <button className='btn'>Get Started</button>
+              <button className='btn'>Learn More</button>
             </div>
         </div>
-        <div className='right-h'>Right</div>
+        <div className='right-h'>
+          <button className='btn'>Join Now</button>
+          <div className='heart-rate'>
+            <img src={Heart}/>
+            <span>Heart Rate</span>
+            <span>116 BPM</span>
+          </div>
+        </div>
     </div>
   )
 }
