ac3868df6e30fcd0553f71e3e8e633ebb297042b
nikhil-pp <nikhil_pp@apple.com>
2024-04-13 10:36:42 +0530

    Progress on logo and headers

--- gym/src/App.js
+++ gym/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Hero from './components/Hero/Hero';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello...</h1>
+     <Hero/>
     </div>
   );
 }
